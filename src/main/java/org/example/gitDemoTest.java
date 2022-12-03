f6b2d84b267cbb058c0ad6c8bdbb58150025b007
shenyue <shen@yue.com>
2022-12-03 14:40:37 +0800

    san test

--- src/main/java/org/example/gitDemoTest.java
+++ src/main/java/org/example/gitDemoTest.java
@@ -17,5 +17,6 @@ public class gitDemoTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git!0.2");
+        System.out.println("hello git!0.3");
     }
 }
