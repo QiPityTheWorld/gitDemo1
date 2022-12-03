653c90c14e662305b02dc9ff735947f557dce7d6
shenyue <shen@yue.com>
2022-12-03 14:38:58 +0800

    tow test

--- src/main/java/org/example/gitDemoTest.java
+++ src/main/java/org/example/gitDemoTest.java
@@ -16,5 +16,6 @@ package org.example;
 public class gitDemoTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!0.2");
     }
 }
