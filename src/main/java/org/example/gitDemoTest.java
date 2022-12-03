4ee5a707c4588f884341bf6b9d68643444e9e841
shenyue <shen@yue.com>
2022-12-03 14:38:01 +0800

    one test

---
+++ src/main/java/org/example/gitDemoTest.java
@@ -0,0 +1,20 @@
+package org.example;
+/**
+ * @author 齐sir
+ * @function 作用
+ * @tell 讲述
+ */
+
+/**
+ *@BelongsProject: gitDemo
+ *@BelongsPackage: org.example
+ *@Author: yanhongwei
+ *@CreateTime: 2022-12-03  14:35
+ *@Description: TODO
+ *@Version: 1.0
+ */
+public class gitDemoTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
